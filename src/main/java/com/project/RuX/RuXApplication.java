605d3ecc64e07ecc2ebc517220d979b5771e7684
afzal.nandolia <afzal.nandolia@gmail.com>
2025-07-13 21:45:14 +0530

    project setup

---
+++ src/main/java/com/project/RuX/RuXApplication.java
@@ -0,0 +1,13 @@
+package com.project.RuX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/RuX/RuXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.RuX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
